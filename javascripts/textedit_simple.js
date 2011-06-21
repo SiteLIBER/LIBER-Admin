18b0577ba99ee701729ec09565a14562d52fdd80
Herond Robaina Salles <hrsalles@republica.(none)>
2011-06-21 01:57:54 -0300

    Adicionado tinymce, jquery e tokeninput em postagem rápida.

---
+++ javascripts/textedit_simple.js
@@ -0,0 +1,18 @@
+tinyMCE.init({
+	language : "pt",
+	mode : "textareas",
+	editor_deselector : "no_editor",
+
+	plugins : "table,media,preview,contextmenu,style",
+
+	theme : "advanced",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_buttons1 : "undo,redo,|,bold,italic,underline,|,sub,sup,|,link,unlink,|,blockquote,|,bullist,numlist,|,image,media,|,code",
+    theme_advanced_buttons2 : "",
+    theme_advanced_buttons3 : "",
+    theme_advanced_statusbar_location: "bottom",
+    theme_advanced_resizing : true,
+    theme_advanced_resize_horizontal : false
+});
+
